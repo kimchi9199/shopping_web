9eb768a344a5615fcefbd6257e2e296386ef4971
Khietlatao <19520628@gm.uit.edu.vn>
2022-05-26 16:26:09 +0700

    commit

---
+++ includes/brands-slider.php
@@ -0,0 +1,85 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Our Brands</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/logo-PNJ.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/THE-GIOI-KIM-CUONG-500X500.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/LOGO-DOJI.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/logo_SBJ.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/logo_SJC.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/logo_skymond.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				
+<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/samsung.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/hcl.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/sony.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/voltas.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/lg.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/lenovo.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+	
+</div><!-- /.logo-slider -->
